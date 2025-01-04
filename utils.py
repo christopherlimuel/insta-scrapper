44709875689a39d3d3f3eaf3555729c93cff2645
christopherlimuel <christopherlimuel@gmail.com>
2025-01-04 11:45:54 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.core.os_manager import ChromeType
 import time
 import csv
 import pickle
@@ -19,6 +21,19 @@ try:
 except FileNotFoundError:
     st.error("File .json tidak ditemukan")
     st.stop()
+
+# Inisialisasi driver
+@st.cache_resource
+def get_driver():
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+    return webdriver.Chrome(
+        service=ChromeService(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
 def login_instagram(driver, username, password):
     driver.get("https://www.instagram.com/accounts/login/")
